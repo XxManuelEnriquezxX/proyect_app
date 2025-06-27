c3068195f19d1f8da7cb554e358528d7f1f81e01
Charlymain <carlosgarcia23ags@gmail.com>
2025-06-27 17:51:39 -0600

    Se guardaron cambios en auth.service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
-
+import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService : JwtService
+  ) {}
+
+  async validateUser(username: string, pass: string): Promise<any> {
+    const user = await this.usersService.findOne(username);
+    //ToDo: comparar contraseña cifrada
+    if (user && user.password === pass) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+    async createJWT(user: any) {
+        const payload = { username: user.username, sub: user.id };
+        return {
+        user,
+        access_token: this.jwtService.sign(payload),
+        };
+    }
+}
