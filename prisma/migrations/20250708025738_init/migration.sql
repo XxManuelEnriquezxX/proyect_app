c1833997c5b9b17cb83659e468174a71f704bbd9
cristhian <cristhiangarciar136@gmail.com>
2025-07-07 21:02:48 -0600

    Modificacion uso de lower case en el modelo

--- prisma/migrations/20250708025738_init/migration.sql
+++ prisma/migrations/20250708025738_init/migration.sql
@@ -1,10 +1,12 @@
 -- CreateTable
-CREATE TABLE `Usuarios` (
+CREATE TABLE `usuarios` (
     `id` VARCHAR(191) NOT NULL,
     `name` VARCHAR(64) NOT NULL,
     `apellidoPaterno` VARCHAR(64) NOT NULL,
     `apellidoMaterno` VARCHAR(64) NOT NULL,
-    `color` VARCHAR(32) NOT NULL,
+    `email` VARCHAR(64) NOT NULL,
+    `suscripto` BOOLEAN NOT NULL,
+    `nombreUsuario` VARCHAR(64) NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
