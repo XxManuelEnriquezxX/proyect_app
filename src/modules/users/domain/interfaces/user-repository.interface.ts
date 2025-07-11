d2485e43cee9159032adccd5940fa44ec6498fd9
Charlymain <carlosgarcia23ags@gmail.com>
2025-07-11 15:07:01 -0600

    case-user completado

--- src/modules/users/domain/interfaces/user-repository.interface.ts
+++ src/modules/users/domain/interfaces/user-repository.interface.ts
@@ -4,4 +4,5 @@ export abstract class IUserRepository{
     abstract crear(user: User): Promise<User>;
     abstract buscarPorId(id:string): Promise<User | null>;
     abstract obtenerTodos(): Promise<User[]>;
+     abstract obtenerTodos(): Promise<User[]>;
 }
